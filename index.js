82cf1374a94efb2224a317194c931f2d6ff1d953
Sebastian <lynchseb@gmail.com>
2020-11-06 20:33:30 +0000

    wiped and re-implemented index.js

--- index.js
+++ index.js
@@ -1,77 +1,94 @@
+
 class Account {
 
   constructor(username) {
     this.username = username;
-    this.transactions = [];
+    this.transactions = []
+
   }
 
   get balance() {
-    let balance = 0;
-    for (let t of this.transactions) {
-    balance += t.value;
-    }
-    return balance;
+   let total = 0
+   this.transactions.forEach((transaction) => {
+    total += transaction.value;
+   })
+   return total;
   }
 
   addTransaction(transaction) {
-    this.transactions.push(transaction);
+    this.transactions.push(transaction)
   }
 
 }
+
 class Transaction {
 
   constructor(amount, account) {
-    this.amount  = amount;
+    this.amount = amount;
     this.account = account;
   }
+
   commit() {
-    if(this.isAllowed() <= 0){
+    if(!this.isAllowed()){
       return false;
     }
-    // Keep track of the time of the transaction
     this.time = new Date();
-    // Add the transaction to the account
-    this.account.addTransaction(this);
+    this.account.addTransaction(this)
+
+
+  }
+}
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount
+  }
+
+  isAllowed() {
+   if (this.account.balance - this.amount <= 0){
+     console.log("Insufficient funds")
+     return false;
+   } else {
+     console.log(`You have withdrawn ${this.amount}, you now have ${this.account.balance - this.amount} left`)
+     return (this.account.balance - this.amount)
+   }
+
+
   }
 
 }
+
 class Deposit extends Transaction {
-    get value () {
-    return this.amount;
 
-    }
-    isAllowed() {
-      return true;
-    }
+  get value() {
+    return this.amount
+  }
+
+  isAllowed(){
+    console.log(`You have deposited ${this.amount}`)
+    return true;
+  }
+
 
 }
-
-class Withdrawal extends Transaction {
-    get value () {
-      return -this.amount;
-    }
-
-    isAllowed() {
-    return(this.account.balance - this.amount >= 0)
-    }
-}
-
-
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const account1 = new Account('billybob');
 
-console.log('Starting Balance:', account1.balance);
+const myAccount = new Account('billybob');
 
-const t1 = new Deposit(120.00, account1);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
 
-const t2 = new Withdrawal(90.00, account1);
+const t2 = new Withdrawal(40, myAccount);
 t2.commit();
 
-console.log('Ending Balance:', account1.balance);
-console.log(account1.transactions)
 
+
+console.log('Ending Balance:', myAccount.balance);
